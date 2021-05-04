7fc8bb993dd6a3b7ced3d2546c9eef5e8c8d840e
Christopher Black <christopher.black.2209@googlemail.com>
2021-05-04 23:42:43 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,13 +4,10 @@ class MP3Importer
   attr_accessor :path
   
   def initialize(file_path)
-<<<<<<< HEAD
     @path = file_path
-=======
     file_path.chomp("./spec")
     #binding.pry
    # @files << file_path
->>>>>>> c2c2398b6900632f7b8f500a11093cb82e05e313
   end 
   
   def files
